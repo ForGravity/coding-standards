4ad85ed616a118307dad0766ef38f072d26c9631
Yoren Chang <yorenchang@gmail.com>
2023-03-30 16:21:17 +0800

    Support eslint with bare config from wp eslint-plugin

---
+++ packages/eslint-config/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'plugin:@wordpress/eslint-plugin/recommended-with-formatting', // Change to
+		// 'plugin:@wordpress/eslint-plugin/recommended' to enable Prettier or TypeScript support.
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	ignorePatterns: [
+		'node_modules/**/.js',
+		'tests/**/.js',
+		'vendor/**/.js',
+	],
+};
