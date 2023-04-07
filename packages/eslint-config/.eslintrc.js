a65c20bebb8ebd48effcdd0d59f245fd08d84991
Yoren Chang <yorenchang@gmail.com>
2023-04-07 20:27:11 +0800

    Add custom rule to allow spaces in objects
    Also, disable no-multiple-spaces errors for variable declarator and import declaraion

--- packages/eslint-config/.eslintrc.js
+++ packages/eslint-config/.eslintrc.js
@@ -1,20 +1,29 @@
 module.exports = {
-	root: true,
-	env: {
+	root:          true,
+	env:           {
 		browser: true,
-		es2021: true,
+		es2021:  true,
 	},
-	extends: [
+	extends:       [
 		'plugin:@wordpress/eslint-plugin/recommended-with-formatting', // Change to
 		// 'plugin:@wordpress/eslint-plugin/recommended' to enable Prettier or TypeScript support.
 	],
 	parserOptions: {
 		ecmaVersion: 'latest',
-		sourceType: 'module',
+		sourceType:  'module',
 	},
-	rules: {
-		camelcase: [
+	rules:         {
+		camelcase:         [
 			'warn',
 		],
+		'no-multi-spaces': [ 'error', {
+			exceptions: {
+				VariableDeclarator: true,
+				ImportDeclaration:  true,
+			},
+		} ],
+		'key-spacing':     [ 'error', {
+			mode: 'minimum', align: 'value',
+		} ],
 	},
 };
